3c56c197481b28129e9f7fadd7acd785412f8b23
nihal-here <92803275+nihal-here@users.noreply.github.com>
2023-11-14 13:27:30 +0530

    completed the script file added all the functions

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const myLibrary = [];
-
 class Book {
   constructor(title, author, page, hasRead) {
     this.title = title;
@@ -9,24 +7,90 @@ class Book {
   }
 }
 
-class Library {
-  constructor(myLibrary) {
-    this.myLibrary = myLibrary;
-  }
+// myLibrary array of books
+const myLibrary = [
+  new Book("The Great Gatsby", "F. Scott Fitzgerald", 218, true),
+  new Book("To Kill a Mockingbird", "Harper Lee", 324, false),
+  new Book("1984", "George Orwell", 328, true),
+];
+
+//function to push books to library
+function addBooktoLibrary(title, author, page, hasRead) {
+  myLibrary.push(new Book(title, author, page, hasRead));
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+//function to display books
+function displayBooks() {
+  const booksGrid = document.querySelector("#booksGrid");
+  booksGrid.innerHTML = "";
+
+  myLibrary.forEach((book) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+    bookCard.innerHTML = `
+        <h2>${book.title}</h2>
+        <p>${book.author}</p>
+        <p>${book.page}</p>
+        <button class='btn ${getButtonClass(
+          book
+        )}' onClick=toggleRead(${myLibrary.indexOf(book)})>${getButtonText(
+      book
+    )}</button>
+        <button class='btn btn-remove' onClick='removeBook(${myLibrary.indexOf(
+          book
+        )})'>Remove</button>
+  `;
+
+    booksGrid.appendChild(bookCard);
+  });
 }
 
+function getButtonClass(book) {
+  return book.hasRead ? "btn-green" : "btn-red";
+}
+
+function getButtonText(book) {
+  return book.hasRead ? "Read" : "Not Read";
+}
+
+function toggleRead(index) {
+  myLibrary[index].hasRead = !myLibrary[index].hasRead;
+  displayBooks();
+}
+
+function clearForm() {
+  document.querySelector("#title").value = "";
+  document.querySelector("#author").value = "";
+  document.querySelector("#pages").value = "";
+  document.querySelector("#hasRead").value = "";
+}
+
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  displayBooks();
+}
 const addBookBtn = document.querySelector("#addBookBtn");
 const addBookDialog = document.querySelector("#addBookDialog");
-const bookTitle = document.querySelector("#title");
-const bookAuthor = document.querySelector("#author");
-const bookPage = document.querySelector("#pages");
-const hasRead = document.querySelector("#hasRead");
-const bookForm = document.querySelector("#bookForm");
-
 addBookBtn.addEventListener("click", () => {
   addBookDialog.showModal();
 });
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const hasRead = document.querySelector("#hasRead").checked;
+
+  addBooktoLibrary(title, author, pages, hasRead);
+  displayBooks();
+
+  addBookDialog.close();
+  clearForm();
+}
+
+const bookForm = document.querySelector("#bookForm");
+bookForm.addEventListener("submit", handleFormSubmit);
+
+displayBooks();
